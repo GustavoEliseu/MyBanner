42cdd48656d0b296968da822318ac4ab6c025592
Gustavo Amaral Eliseu <gustavo_eliseu@hotmail.com>
2017-08-10 18:48:14 -0300

    Initial commit

---
+++ app/src/main/java/meuprimeiroapp/studio/gustavo/mybanner/MainActivity.java
@@ -0,0 +1,31 @@
+package meuprimeiroapp.studio.gustavo.mybanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ss.com.bannerslider.banners.Banner;
+import ss.com.bannerslider.banners.DrawableBanner;
+import ss.com.bannerslider.banners.RemoteBanner;
+import ss.com.bannerslider.views.BannerSlider;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BannerSlider bannerSlider = (BannerSlider) findViewById(R.id.banner_slider1);
+        List<Banner> banners=new ArrayList<>();
+        //add banner using image url
+        //banners.add(new RemoteBanner("Put banner image url here ..."));
+        //add banner using resource drawable
+        banners.add(new DrawableBanner(R.drawable.text));
+        banners.add(new DrawableBanner(R.drawable.text150));
+        bannerSlider.setBanners(banners);
+
+    }
+}
